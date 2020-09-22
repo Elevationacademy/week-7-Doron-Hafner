698540295d9232db5f4df96c45199dc3b5ef9406
Doron Hafner <dh9707@gmail.com>
2020-09-22 20:50:43 +0300

    data structure ex1

---
+++ day2/DataStructurs/Exercises.js
@@ -0,0 +1,33 @@
+class UniqueArray {
+    constructor () {
+        this.arr = []
+        this.items = {}
+    }
+
+    add(item) {
+        !this.items[item] ? (this.items[item] = true, this.arr.push(item)) : 'x'
+    }
+
+    showAll() {
+        console.log(Object.keys(this.items));
+    }
+
+    exists(item) {
+        return this.items[item] ? true : false
+    }
+
+    get(index) {
+         return this.arr[index] ? this.arr[index] : -1
+    }
+}
+
+
+const uniqueStuff = new UniqueArray()
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.exists("toy") //returns true
+uniqueStuff.add("poster")
+uniqueStuff.add("hydrogen")
+console.log(uniqueStuff.get(2)) //prints "hydrogen"
